58ba4081b684352533eff78173ce461b56773349
Jacob Boodry <jake.boodry@live.com>
2018-06-21 18:58:09 +0000

    automatically backed up by learn

--- objects.js
+++
@@ -1,6 +0,0 @@
-function updateObjectWithKeyAndValue(obj, key, value) {
- 
-  return Object.assign({}, obj, { [key]: value })
-  console.log(updateObjectWithKeyAndValue)
-}
-console.log(updateObjectWithKeyAndValue(Yes, Y, YESEs))
